8a164b1cb41012af1c3c0737e24813168eda078c
kazuyan <komokazu.nankai@gmail.com>
2024-11-21 00:43:43 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, g
+import sqlite3
+
+app = Flask(__name__)
+DATABASE = 'sample.db'
+
+# データベース接続を管理する関数
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
